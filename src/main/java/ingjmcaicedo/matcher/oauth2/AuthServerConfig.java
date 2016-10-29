059ebef48e16eebdcfdd78b561ec936d0df40ed4
Juan Manuel Caicedo <jmcaicedo1@gmail.com>
2016-10-28 21:08:19 -0500

    Importing files

---
+++ src/main/java/ingjmcaicedo/matcher/oauth2/AuthServerConfig.java
@@ -0,0 +1,13 @@
+package ingjmcaicedo.matcher.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerConfig {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(AuthServerConfig.class, args);
+	}
+	
+}
